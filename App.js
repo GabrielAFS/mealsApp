e977adee5335370bd5cb26e71903f3bdfda75dd0
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2020-10-12 21:50:57 -0300

    feat: adds redux logic

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { enableScreens } from 'react-native-screens'; // Versões mais recentes do expo e do react native
+import React, { useState } from "react";
+import { StatusBar } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { enableScreens } from "react-native-screens"; // Versões mais recentes do expo e do react native
 // import { useScreens } from 'react-native-screens';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
-import MealsNavigator from './navigation/MealsNavigator';
+import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
+
+import Colors from './constants/colors';
 
 enableScreens();
 
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
+
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  })
-}
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false)
+  const [fontLoaded, setFontLoaded] = useState(false);
 
   if (!fontLoaded) {
     return (
@@ -25,17 +36,16 @@ export default function App() {
         startAsync={fetchFonts}
         onFinish={() => setFontLoaded(true)}
       />
-    )
+    );
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={Colors.primary}
+      />
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
